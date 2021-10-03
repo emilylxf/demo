f8023547571c990b235804eb41c785a829c3cf58
Emily <xiaofang2146@gmail.com>
2021-10-03 15:26:37 -0700

    deploy

---
+++ server.js
@@ -0,0 +1,11 @@
+const { RSA_NO_PADDING } = require("constants");
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = 8000;
+
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
